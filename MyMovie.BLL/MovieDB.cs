be9ccd68739d9c7389614653eacd153f365280f5
caoludogy <caolujob@126.com>
2016-05-22 20:59:00 +0800

    增加遗忘的文件

---
+++ MyMovie.BLL/MovieDB.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MyMovie.Model;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+using System.Data.Common;
+using System.Data;
+
+namespace MyMovie.BLL
+{
+    public class MovieDB
+    {
+        public MovieDetailModel GetDetail(int id)
+        {
+            string sql = "select m.id,d.typename,m.name,m.movieurl,m.introduce,m.actors";
+            sql += " from [Movies] m";
+            sql += " inner join dbo.DicType d on m.typename=d.typeid ";
+            sql += " where id=" + id;
+
+            MovieDetailModel model = new MovieDetailModel();
+            try
+            {
+                Database database = DatabaseFactory.CreateDatabase("MainConnection");
+                using (DbCommand cmd = database.GetSqlStringCommand(sql))
+                {
+                    using (IDataReader reader = database.ExecuteReader(cmd))
+                    {
+                        while (reader.Read())
+                        {
+                            model = new MovieDetailModel(reader);
+                        }
+                    }
+                }
+                return model;
+            }
+            catch (Exception ex)
+            {
+                return new MovieDetailModel();
+            }
+        }
+        
+        public List<MovieDetailModel> GetListByTypeName(string typename)
+        {
+            string sql = "select m.id,d.typename as typename,m.name,m.[MovieImg]";
+            sql += " from [Movies] m";
+            sql += " inner join dbo.DicType d on m.typename=d.typeid ";
+            sql += " where m.typename='" + typename + "'";
+
+            List<MovieDetailModel> list = new List<MovieDetailModel>();
+            try
+            {
+                Database database = DatabaseFactory.CreateDatabase("MainConnection");
+                using (DbCommand cmd = database.GetSqlStringCommand(sql))
+                {
+                    using (IDataReader reader = database.ExecuteReader(cmd))
+                    {
+                        while (reader.Read())
+                        {
+                            list.Add(new MovieDetailModel(reader));
+                        }
+                    }
+                }
+                return list;
+            }
+            catch (Exception ex)
+            {
+                return new List<MovieDetailModel>();
+            }
+        }
+    }
+}
---
+++ MyMovie.BLL/UserDB.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+using System.Data.Common;
+using System.Data;
+
+namespace MyMovie.BLL
+{
+    public class UserDB
+    {
+        public int CheckUserName(string userName)
+        {
+            string sql = "select u.id from users u where username='" + userName + "'";
+
+            int id = 0;
+            try
+            {
+                Database database = DatabaseFactory.CreateDatabase("MainConnection");
+                using (DbCommand cmd = database.GetSqlStringCommand(sql))
+                {
+                    using (IDataReader reader = database.ExecuteReader(cmd))
+                    {
+                        while (reader.Read())
+                        {
+                            id = reader.GetInt32(0);
+                        }
+                    }
+                }
+                return id;
+            }
+            catch (Exception ex)
+            {
+                return 0;
+            }
+        }
+
+        public int SaveUser(string userName, string password)
+        {
+            string sql = "insert into users(username,password) values('" + userName + "','" + password + "')";
+            int id = 0;
+            try
+            {
+                Database database = DatabaseFactory.CreateDatabase("MainConnection");
+                using (DbCommand cmd = database.GetSqlStringCommand(sql))
+                {
+                    database.ExecuteNonQuery(cmd);
+                    id = 0;
+                }
+                return id;
+            }
+            catch (Exception ex)
+            {
+                return 0;
+            }
+        }
+        public int Check(string userName, string password)
+        {
+            string sql = "select u.id from users u where username='"+ userName +"' and password ='" + password +"'";
+                 int id = 0;
+            try
+            {
+                Database database = DatabaseFactory.CreateDatabase("MainConnection");
+                using (DbCommand cmd = database.GetSqlStringCommand(sql))
+                {
+                    using (IDataReader reader = database.ExecuteReader(cmd))
+                    {
+                        while (reader.Read())
+                        {
+                            id = reader.GetInt32(0);
+                        }
+                    }
+                }
+                return id;
+            }
+            catch (Exception ex)
+            {
+                return 0;
+            }
+        }
+
+        public string GetUserName(int userID)
+        {
+            string sql = "select u.username from users u where id='" + userID + "'";
+
+            string id = "";
+            try
+            {
+                Database database = DatabaseFactory.CreateDatabase("MainConnection");
+                using (DbCommand cmd = database.GetSqlStringCommand(sql))
+                {
+                    using (IDataReader reader = database.ExecuteReader(cmd))
+                    {
+                        while (reader.Read())
+                        {
+                            id = reader.GetString(0);
+                        }
+                    }
+                }
+                return id;
+            }
+            catch (Exception ex)
+            {
+                return "";
+            }
+        }
+    }
+}
